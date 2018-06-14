95804014191936f9999df8326110d1babcd17c55
Jason Juhyun Kim <mtinet@hanmail.net>
2018-06-14 15:02:05 +0900

    Create test3.ino

---
+++ code/test3.ino
@@ -0,0 +1,36 @@
+int enA = 3;
+int in1 = 12;
+
+void setup() {
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+}
+
+void loop() {
+  slowRunStop();
+}
+
+void slowRunStop() {
+  for(int i = 0; i < 256; i++) {
+    digitalWrite(in1, HIGH);
+    analogWrite(enA, i);
+    delay(10);
+  }
+  
+  for(int i = 255; i > 0; i--) {
+    digitalWrite(in1, HIGH);
+    analogWrite(enA, i);
+    delay(10);
+  }
+  for(int i = 0; i < 256; i++) {
+    digitalWrite(in1, LOW);
+    analogWrite(enA, i);
+    delay(10);
+  }
+  for(int i = 255; i > 0; i--) {
+    digitalWrite(in1, LOW);
+    analogWrite(enA, i);
+    delay(10);
+  }
+}
