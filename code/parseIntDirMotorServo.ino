cb2b9b7e5eeae69795a6c8a7e90e85b7eaa55323
Jason Juhyun Kim <mtinet@hanmail.net>
2018-06-18 14:49:34 +0900

    Update parseIntDirMotorServo.ino

--- code/parseIntDirMotorServo.ino
+++ code/parseIntDirMotorServo.ino
@@ -1,9 +1,12 @@
-// 시리얼을 읽은 다음 parseInt()함수를 사용해 각각의 변수에 나누어 넣은 다음 그 변수의 값에 해당하는 방향, 모터속도, 서보모터 조향값으로 입력함  
+// 시리얼을 읽은 다음 parseInt()함수를 사용해 각각의 변수에 나누어 넣은 다음 그 변수의 값에 해당하는 방향, 모터속도, 서보모터 조향값으로 입력함
+// 1 == forward, 2 == backward, 0 == stop  
+// 데이터 형식(ex. 1 90 255)
 
 #include <Servo.h>
 
 int enA = 3;
-int in1 = 12;
+int in1 = 12; 
+int in2 = 9; //brake
 int ledRed = 13;
 int ledBlue = 5;
 
@@ -17,6 +20,7 @@ void setup() {
   // set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
   
   pinMode(ledRed, OUTPUT);
   
@@ -47,7 +51,7 @@ void loop() {
     if (dir == 2) {
       backwardCar(dir, valServo, valMotor);
     }
-    if (dir == 3) {
+    if (dir == 0) {
       stopCar(valServo);
     }       
   }
@@ -71,7 +75,8 @@ void stopCar(int valServo) {
   myServo.write(valServo);
   digitalWrite(ledRed, LOW);
   digitalWrite(ledBlue, LOW);
-  digitalWrite(in1, HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
   analogWrite(enA, 0);
 }
   
